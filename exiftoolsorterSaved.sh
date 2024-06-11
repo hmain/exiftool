d1b399731ff81ce911fd0c5c5dd42f8b798bd677
Hamin Mousavi <hmain@users.noreply.github.com>
2024-06-11 20:53:07 +0200

    Exiftool scripts

---
+++ exiftoolsorterSaved.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Requirements
+#pkg install -y bash p5-Image-ExifTool
+
+# INFO  Copy photos based on Year/Year-Month-Day from syncthing phone camera folder
+# -o ~/dummy This flag is to copy, not move. The directory is a fallback if the flag isn't available on the given photo. Good if using something like DateTimeOriginal
+# -d /mnt/Photos/%Y/%Y-%m-%d Create folders with the given date format.
+# -r Run recursively
+
+set -e
+
+EXIFTOOL="/usr/local/bin/exiftool"
+
+# Copy jpegs to directory recursively into organized folder based on FileModifyDate
+$EXIFTOOL -o ~/dummy/ "-Directory<FileModifyDate" -d "/data/Photos/%Y/%Y-%m-%d" -r /data/Misc/syncthing/savedphotos/ -ext jpg
+
+#&&
+# Copy mp4's based on CreateDate into organized folder
+#$EXIFTOOL -o ~/dummy/ "-Directory<CreateDate" -d "/mnt/Photos/%Y/%Y-%m-%d" -r /mnt/syncthing/phonecamera/ -ext mp4
