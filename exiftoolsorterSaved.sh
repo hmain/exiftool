d8f661d2cc588ba0820f70b32165e924a2ed0908
Hamin Mousavi <hmain@users.noreply.github.com>
2024-06-11 22:18:41 +0200

    Update exiftoolsorterSaved.sh

--- exiftoolsorterSaved.sh
+++ exiftoolsorterSaved.sh
@@ -5,6 +5,7 @@
 
 # INFO  Copy photos based on Year/Year-Month-Day from syncthing phone camera folder
 # -o ~/dummy This flag is to copy, not move. The directory is a fallback if the flag isn't available on the given photo. Good if using something like DateTimeOriginal
+# -m Ignore minor errors
 # -d /mnt/Photos/%Y/%Y-%m-%d Create folders with the given date format.
 # -r Run recursively
 
@@ -13,6 +14,6 @@ set -e
 EXIFTOOL="/usr/bin/exiftool"
 
 # Copy jpegs to directory recursively into organized folder based on FileModifyDate
-$EXIFTOOL -o ~/dummy/ "-Directory<FileModifyDate" -d "/data/Photos/%Y/%Y-%m-%d" -r /data/Misc/syncthing/savedphotos/ -ext jpg
+$EXIFTOOL -o ~/dummy/ "-Directory<FileModifyDate" -m -d "/data/Photos/%Y/%Y-%m-%d" -r /data/Misc/syncthing/savedphotos/ -ext jpg
 
 exit 0
